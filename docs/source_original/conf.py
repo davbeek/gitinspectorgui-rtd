dedac62f6788c54d63b28f7d63b1624a7879f91e
Bert van Beek <d.a.v.beek@tue.nl>
2023-06-10 17:55:14 +0200

    Rename original source folder

--- docs/source_original/conf.py
+++ docs/source_original/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'gitinspectorgui'
+copyright = 'The gitinspectorgui Team'
+author = 'Bert van Beek'
 
 release = '0.1'
 version = '0.1.0'
